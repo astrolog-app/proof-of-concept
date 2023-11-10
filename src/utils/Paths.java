6fbce21e9536ec8600b17d786754d4e99d5f29f0
Rouven Spaar <rouven.spaar@gmail.com>
2023-11-10 07:08:40 +0100

    improved file path

--- src/utils/Paths.java
+++ src/utils/Paths.java
@@ -1,9 +1,11 @@
 package utils;
 
+import java.io.File;
+
 public class Paths {
     private static final String PROJECT_PATH = System.getProperty("user.dir");
     public static final String APP_PATH = (
-            PROJECT_PATH.replace("\\AstroLogger-code", "") + "\\AstroLogger-app");
-    public static final String CONFIGURATION_PATH = APP_PATH + "\\configuration.json";
-    public static final String IMAGE_PATH = APP_PATH + "\\images\\";
+            PROJECT_PATH.replace("AstroLogger-code", "") + "AstroLogger-app");
+    public static final String CONFIGURATION_PATH = APP_PATH + File.separator + "configuration.json";
+    public static final String IMAGE_PATH = APP_PATH + File.separator + "images" + File.separator;
 }
