77d6d0ebc225b26e2fa3e4c2bf6037f01da1bcc4
Rouven Spaar <rouven.spaar@gmail.com>
2023-11-14 19:04:07 +0100

    -

--- src/ui/corecomponents/LogPanel.java
+++ src/ui/corecomponents/LogPanel.java
@@ -1,8 +1,15 @@
 package ui.corecomponents;
 
+import models.settings.AppConfiguration;
+import services.fileHandler.ConfigurationStore;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LogPanel {
+    AppConfiguration appConfig;
+    ConfigurationStore configStore;
     private JPanel panel1;
     private JLabel placeHolder1;
     private JButton fromExistingFolderButton;
@@ -11,6 +18,22 @@ public class LogPanel {
     private JCheckBox enableRegularBackupsCheckBox;
     private JButton configureRegularBackupsButton;
     private JButton saveBackupConfigButton;
+
+    public LogPanel(AppConfiguration appConfig, ConfigurationStore configStore) {
+        this.appConfig = appConfig;
+        this.configStore = configStore;
+
+        backupHandler();
+    }
+
+    private void backupHandler() {
+        enableRegularBackupsCheckBox.setSelected(appConfig.getEnableRegularBackups());
+
+        enableRegularBackupsCheckBox.addActionListener(e -> {
+            appConfig.setEnableRegularBackups(enableRegularBackupsCheckBox.isSelected());
+            configStore.save(null);
+        });
+    }
 
     public JPanel getPanel() {
         return panel1;
