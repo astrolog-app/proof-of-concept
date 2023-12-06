25bb7246758331083747066f7dc3e72eb597e549
Rouven Spaar <rouven.spaar@gmail.com>
2023-12-06 13:24:53 +0100

    -

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
 import services.AppActions;
-import services.fileHandler.ConfigurationStore;
 
 public class App {
     public static void main(String[] args) {
-//        AppActions appActions = new AppActions();
-//
-//        appActions.initialize();
+        AppActions appActions = new AppActions();
 
-        ConfigurationStore.load(null);
+        appActions.initialize();
     }
 }
