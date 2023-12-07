dd119faa904b67f36a1918fe907503e50cdf16a1
Rouven Spaar <rouven.spaar@gmail.com>
2023-12-07 21:11:01 +0100

    -

--- src/ui/popUps/NewTelescopePanel.java
+++ src/ui/popUps/NewTelescopePanel.java
@@ -4,8 +4,8 @@ import models.equipment.Equipment;
 import models.equipment.Telescope;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
 
 public class NewTelescopePanel extends JDialog {
@@ -21,8 +21,11 @@ public class NewTelescopePanel extends JDialog {
         List<String> brandList = new ArrayList<>(equipment.getAllBrands());
         Collections.sort(brandList);
 
-        brandField.addItem("Select Brand");
-        brandField.addItem("Add New");
+        setFontsAndColorsForItems(brandField);
+
+        brandField.addItem(null);
+        //brandField.addItem("Select Brand");
+        brandField.addItem("Add New Brand");
         for (String brand : brandList) {
             brandField.addItem(brand);
         }
@@ -38,6 +41,8 @@ public class NewTelescopePanel extends JDialog {
             dispose();
         });
 
+        brandField.addActionListener(e -> brandField.removeItem(null));
+
         cancelButton.addActionListener(e -> this.dispose());
 
         setModal(true);
@@ -47,5 +52,46 @@ public class NewTelescopePanel extends JDialog {
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private static void setFontsAndColorsForItems(JComboBox<String> comboBox) {
+        // Create a map to store fonts and colors for each item
+        Map<String, Map<String, Object>> itemStyles = new HashMap<>();
+
+        // Set custom fonts and colors for specific items
+       // Map<String, Object> styleItem1 = new HashMap<>();
+       // styleItem1.put("Font", new Font(null, Font.PLAIN, 12));
+       // styleItem1.put("Color", null);
+       // itemStyles.put("Select Brand", styleItem1);
+
+        Map<String, Object> styleItem2 = new HashMap<>();
+        styleItem2.put("Font", new Font(null, Font.BOLD, 14));
+        styleItem2.put("Color", null);
+        itemStyles.put("Add New Brand", styleItem2);
+
+        // Set the custom renderer for the JComboBox
+        comboBox.setRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+                // Set the font and color based on the item
+                if (value != null && itemStyles.containsKey(value.toString())) {
+                    Map<String, Object> style = itemStyles.get(value.toString());
+
+                    // Set the font
+                    if (style.containsKey("Font")) {
+                        component.setFont((Font) style.get("Font"));
+                    }
+
+                    // Set the color
+                    if (style.containsKey("Color")) {
+                        component.setForeground((Color) style.get("Color"));
+                    }
+                }
+
+                return component;
+            }
+        });
     }
 }
