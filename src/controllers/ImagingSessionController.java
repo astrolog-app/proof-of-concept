15d559822478b1d2ff7e6229daa23c1380c969da
Rouven Spaar <rouven.spaar@gmail.com>
2023-12-20 16:04:25 +0100

    new yes_no option

--- src/controllers/ImagingSessionController.java
+++ src/controllers/ImagingSessionController.java
@@ -3,6 +3,8 @@ package controllers;
 import models.equipment.Equipment;
 import ui.popUps.ImagingSessionInfo;
 
+import javax.swing.*;
+
 public class ImagingSessionController {
     public void addImagingSessionManually(Equipment equipment) {
         new ui.popUps.NewImagingSessionManually(equipment);
@@ -13,7 +15,19 @@ public class ImagingSessionController {
     }
 
     public void removeImagingSession() {
+        // hardcode YES_NO_OPTION to disable translation
+        UIManager.put("OptionPane.yesButtonText", "Yes");
+        UIManager.put("OptionPane.noButtonText", "No");
 
+        int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this session?",
+                "Delete Session Confirmation",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.WARNING_MESSAGE
+        );
+
+        if (dialogResult == JOptionPane.YES_NO_OPTION) {
+            // TODO: remove imagingSession
+        }
     }
 
     public void editImagingSession() {
