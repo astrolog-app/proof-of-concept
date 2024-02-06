d5f9c8e3c2ec4ec6c532a0e9357b40828c7afa84
Rouven Spaar <rouven.spaar@gmail.com>
2024-02-06 07:40:49 +0100

    added ui for startUpPanel

--- src/ui/startUp/StartUpPanel.java
+++ src/ui/startUp/StartUpPanel.java
@@ -1,10 +1,15 @@
 package ui.startUp;
 
+import utils.Paths;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class StartUpPanel extends JDialog {
     private JPanel mainPanel;
+    private JPanel backgroundPanel;
     private JProgressBar progressBar1;
+
     public StartUpPanel() {
         setUndecorated(true);
         setVisible(true);
@@ -12,5 +17,19 @@ public class StartUpPanel extends JDialog {
         setResizable(false);
         setLocationRelativeTo(null);
         add(mainPanel);
+        progressBar1.setValue(20);
+    }
+
+    private void createUIComponents() {
+        backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Load the background image
+                ImageIcon backgroundImage = new ImageIcon(Paths.IMAGE_PATH + "start_up_background.png");
+                // Draw the background image
+                g.drawImage(backgroundImage.getImage(), 0, -55, getWidth(), 410, null); // TODO: correct image ratio
+            }
+        };
     }
 }
