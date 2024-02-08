b9c0fbba72b8b795b3f4047447142dec84fad8a8
Rouven Spaar <rouven.spaar@gmail.com>
2024-02-08 09:24:59 +0100

    added FileChooser

--- src/ui/popUps/LibraryRowEditor.java
+++ src/ui/popUps/LibraryRowEditor.java
@@ -2,6 +2,7 @@ package ui.popUps;
 
 import models.calibrationLibrary.CalibrationLibrary;
 import models.calibrationLibrary.CalibrationType;
+import models.equipment.Camera;
 import models.equipment.Equipment;
 import models.equipment.EquipmentType;
 import models.tableModels.LibraryTableModel;
@@ -10,6 +11,7 @@ import ui.customComponents.CustomComboBox;
 
 import javax.swing.*;
 import java.awt.event.ItemEvent;
+import java.io.File;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,6 +20,7 @@ public class LibraryRowEditor extends JDialog {
     private final Equipment equipment;
     private final List<CalibrationLibrary> library;
     private final LibraryTableModel tableModel;
+    private String prevPath = "";
     private String prevCamera = "";
     private String prevCalibrationType = "Flat";
     private int prevGain = 0;
@@ -31,6 +34,8 @@ public class LibraryRowEditor extends JDialog {
     private JSpinner gain;
     private JSpinner subLength;
     private JSpinner totalSubs;
+    private JTextField pathField;
+    private JButton changeButton;
 
     public LibraryRowEditor(CalibrationLibrary libraryRow, Equipment equipment, List<CalibrationLibrary> library, LibraryTableModel tableModel) {
         this.libraryRow = libraryRow;
@@ -50,7 +55,7 @@ public class LibraryRowEditor extends JDialog {
         } else {
             setTitle("Add New Library Entry");
         }
-        setSize(400, 300);
+        setSize(500, 350);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -58,17 +63,21 @@ public class LibraryRowEditor extends JDialog {
 
     private void updateButtonState() {
         boolean notNull = this.camera.getSelectedItem() != null;
-        boolean camera = this.camera.getSelectedItem() != prevCamera;
-        boolean calibrationType = this.calibrationType.getSelectedItem() != prevCalibrationType;
+        boolean path = !pathField.getText().equals(prevPath);
+        boolean camera = !this.camera.getSelectedItem().equals(prevCamera);
+        boolean calibrationType = !Objects.equals(this.calibrationType.getSelectedItem(), prevCalibrationType);
         boolean gain = !this.gain.getValue().equals(prevGain);
         boolean subLength = !this.subLength.getValue().equals(prevSubLength);
         boolean totalSubs = !this.totalSubs.getValue().equals(prevTotalSubs);
 
-        saveButton.setEnabled(notNull && (camera || calibrationType || gain || subLength || totalSubs));
+        saveButton.setEnabled(notNull && (path || camera || calibrationType || gain || subLength || totalSubs));
     }
 
     private void fillUpUI() {
         if (libraryRow != null) {
+            pathField.setText(libraryRow.getPath());
+            prevPath = libraryRow.getPath();
+
             camera.setSelectedItem(libraryRow.getCamera(equipment).getViewName());
             prevCamera = libraryRow.getCamera(equipment).getViewName();
 
@@ -97,7 +106,7 @@ public class LibraryRowEditor extends JDialog {
         saveButton.addActionListener(e -> {
             CalibrationLibrary calibrationLibrary = new CalibrationLibrary();
 
-            calibrationLibrary.setPath("");
+            calibrationLibrary.setPath(pathField.getText());
             calibrationLibrary.setCameraId(camera.getSelectedEquipmentItem().getId());
             System.out.println(camera.getSelectedEquipmentItem().getId());
             System.out.println(calibrationLibrary.getId());
@@ -130,6 +139,20 @@ public class LibraryRowEditor extends JDialog {
         gain.addChangeListener(e -> updateButtonState());
         subLength.addChangeListener(e -> updateButtonState());
         totalSubs.addChangeListener(e -> updateButtonState());
+
+        changeButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Select Folder");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            int userSelection = fileChooser.showDialog(null, "Select");
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File selectedFolder = fileChooser.getSelectedFile();
+                updateButtonState();
+                prevPath = selectedFolder.getAbsolutePath();
+                pathField.setText(prevPath);
+            }
+        });
     }
 
     private void createUIComponents() {
