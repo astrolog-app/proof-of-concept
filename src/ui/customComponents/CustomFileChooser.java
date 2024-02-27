ce3440befc64c151ec6daa57ea9d7df63550462c
Rouven Spaar <rouven.spaar@gmail.com>
2024-02-27 16:39:17 +0100

    new CustomFileChooser

---
+++ src/ui/customComponents/CustomFileChooser.java
@@ -0,0 +1,41 @@
+package ui.customComponents;
+
+import models.settings.AppConfig;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class CustomFileChooser extends JPanel {
+    private final AppConfig appConfig;
+    private final JTextField pathField = new JTextField();
+    private final JButton changeButton = new JButton("Change");
+
+    public CustomFileChooser(AppConfig appConfig, String label) {
+        this.appConfig = appConfig;
+
+        setLayout(new BorderLayout(10, 0));
+        pathField.setEnabled(false);
+
+        add(new JLabel(label), BorderLayout.WEST);
+        add(pathField, BorderLayout.CENTER);
+        add(changeButton, BorderLayout.EAST);
+
+        handleActions();
+    }
+
+    private void handleActions() {
+        changeButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Select Folder");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            int userSelection = fileChooser.showDialog(null, "Select");
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                File selectedFolder = fileChooser.getSelectedFile();
+                pathField.setText(selectedFolder.getAbsolutePath().replace(appConfig.getFolderPath(),""));
+                // TODO: updateButtonState
+            }
+        });
+    }
+}
